96e0686051f9a95173d3cd677026debfc3846408
snt-nbzw <sntnbzw@gmail.com>
2021-03-10 23:48:20 +0900

    Improve: move the widget using gestures

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,61 @@ void main() {
 class PhysicsCardDragDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: DraggableCard(
+        child: FlutterLogo(
+          size: 128,
+        ),
+      ),
+    );
+  }
+}
+
+class DraggableCard extends StatefulWidget {
+  DraggableCard({required this.child});
+
+  final Widget child;
+  @override
+  _DraggableCardState createState() => _DraggableCardState();
+}
+
+class _DraggableCardState extends State<DraggableCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  Alignment _dragAlignment = Alignment.center;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onPanDown: (details) {},
+      onPanUpdate: (details) {
+        setState(() {
+          _dragAlignment += Alignment(
+            details.delta.dx / (size.width / 2),
+            details.delta.dy / (size.height / 2),
+          );
+        });
+      },
+      onPanEnd: (details) {},
+      child: Align(
+        alignment: _dragAlignment,
+        child: Card(child: widget.child),
+      ),
+    );
   }
 }
